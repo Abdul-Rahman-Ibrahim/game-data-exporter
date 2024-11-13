be1e39abca68b7550a71ae8b1325204a10615cef
Abdul-Rahman Ibrahim <abdulrahmanibrahim.ish@gmail.com>
2024-11-13 17:19:32 +0300

    added try and except block

--- elmas.py
+++ elmas.py
@@ -39,106 +39,109 @@ if __name__ == '__main__':
         with open(file_path, 'r') as f:
             data = json.load(f)
         
-        games = []
-        for key in data.keys():
-            games.append(Game(key, data))
+        try:
+            games = []
+            for key in data.keys():
+                games.append(Game(key, data))
 
-        for game in games:
-            game_ID = game.ID
-            HOME = game.home
-            selections = HOME.selections
-            if selections:
-                selection = selections[0]
-            else:
-                continue
-
-            STAGE2 = game.stage2
-            STAGE3 = game.stage3
-
-            gametype = selection.gametype
-            player1data = selection.player1data
-            student_id_p1, cinsiyet_p1, studentname_p1, gametype_p1, grade_name_p1, grup_p1, school_name_p1, variation_p1 = (None,) * 8
-            if player1data is not None and type(player1data) is not bool:
-                gametype_p1, cinsiyet_p1, grade_name_p1, student_id_p1, studentname_p1, school_name_p1 = get_player_data(player1data)
-
-            player2data = selection.player2data
-            student_id_p2, cinsiyet_p2, studentname_p2, gametype_p2, grade_name_p2, grup_p2, school_name_p2, variation_p2 = (None,) * 8
-            if player2data is not None and type(player2data) is not bool:
-                gametype_p2, cinsiyet_p2, grade_name_p2, student_id_p2, studentname_p2, school_name_p2 = get_player_data(player2data)
-
-            playerdata = selection.playerdata
-            student_id_p, cinsiyet_p, studentname_p, gametype_p, grade_name_p, grup_p, school_name_p, variation_p = (None,) * 8
-            if playerdata is not None and type(playerdata) is not bool:
-                gametype_p, cinsiyet_p, grade_name_p, student_id_p, studentname_p, school_name_p = get_player_data(playerdata)
-            
-            if gametype == 'cooperative':
-                if student_id_p1 == student_id_p2:
+            for game in games:
+                game_ID = game.ID
+                HOME = game.home
+                selections = HOME.selections
+                if selections:
+                    selection = selections[0]
+                else:
                     continue
 
-            if student_id_p1 is None and student_id_p2 is None:
-                student_id_p1, student_id_p2 = student_id_p, student_id_p
-            if cinsiyet_p1 is None and cinsiyet_p2 is None:
-                cinsiyet_p1, cinsiyet_p2 = cinsiyet_p, cinsiyet_p
-            if school_name_p1 is None and school_name_p2 is None:
-                school_name_p1, school_name_p2 = school_name_p, school_name_p
-            if grade_name_p1 is None and grade_name_p2 is None:
-                grade_name_p1, grade_name_p2 = grade_name_p, grade_name_p
-            
-            if (student_id_p1 and student_id_p2 and cinsiyet_p1 and cinsiyet_p2):
+                STAGE2 = game.stage2
+                STAGE3 = game.stage3
 
-                student_data2 = get_init_row(game_ID, student_id_p1, student_id_p2, cinsiyet_p1, cinsiyet_p2, grade_name_p1, grade_name_p2, gametype, school_name_p1, school_name_p2, GAME_NAME)
-                stage2_data = get_stage_data(STAGE2.player1levels, gametype, 2)
-                rows = get_rows(stage2_data, 1, NUM_LEVELS)
-                for exe_no, lst in rows.items():
-                    STAGE2DATA.append(student_data2 + lst)         
+                gametype = selection.gametype
+                player1data = selection.player1data
+                student_id_p1, cinsiyet_p1, studentname_p1, gametype_p1, grade_name_p1, grup_p1, school_name_p1, variation_p1 = (None,) * 8
+                if player1data is not None and type(player1data) is not bool:
+                    gametype_p1, cinsiyet_p1, grade_name_p1, student_id_p1, studentname_p1, school_name_p1 = get_player_data(player1data)
+
+                player2data = selection.player2data
+                student_id_p2, cinsiyet_p2, studentname_p2, gametype_p2, grade_name_p2, grup_p2, school_name_p2, variation_p2 = (None,) * 8
+                if player2data is not None and type(player2data) is not bool:
+                    gametype_p2, cinsiyet_p2, grade_name_p2, student_id_p2, studentname_p2, school_name_p2 = get_player_data(player2data)
+
+                playerdata = selection.playerdata
+                student_id_p, cinsiyet_p, studentname_p, gametype_p, grade_name_p, grup_p, school_name_p, variation_p = (None,) * 8
+                if playerdata is not None and type(playerdata) is not bool:
+                    gametype_p, cinsiyet_p, grade_name_p, student_id_p, studentname_p, school_name_p = get_player_data(playerdata)
+                
                 if gametype == 'cooperative':
+                    if student_id_p1 == student_id_p2:
+                        continue
+
+                if student_id_p1 is None and student_id_p2 is None:
+                    student_id_p1, student_id_p2 = student_id_p, student_id_p
+                if cinsiyet_p1 is None and cinsiyet_p2 is None:
+                    cinsiyet_p1, cinsiyet_p2 = cinsiyet_p, cinsiyet_p
+                if school_name_p1 is None and school_name_p2 is None:
+                    school_name_p1, school_name_p2 = school_name_p, school_name_p
+                if grade_name_p1 is None and grade_name_p2 is None:
+                    grade_name_p1, grade_name_p2 = grade_name_p, grade_name_p
+                
+                if (student_id_p1 and student_id_p2 and cinsiyet_p1 and cinsiyet_p2):
+
                     student_data2 = get_init_row(game_ID, student_id_p1, student_id_p2, cinsiyet_p1, cinsiyet_p2, grade_name_p1, grade_name_p2, gametype, school_name_p1, school_name_p2, GAME_NAME)
                     stage2_data = get_stage_data(STAGE2.player1levels, gametype, 2)
-                    rows = get_rows(stage2_data, 2, NUM_LEVELS)
+                    rows = get_rows(stage2_data, 1, NUM_LEVELS)
                     for exe_no, lst in rows.items():
-                        STAGE2DATA.append(student_data2 + lst)     
-                elif gametype == 'competitive':
-                    if not all(i is None for i in STAGE2.player2levels) and not all(i is None for i in STAGE3.player2levels):
+                        STAGE2DATA.append(student_data2 + lst)         
+                    if gametype == 'cooperative':
                         student_data2 = get_init_row(game_ID, student_id_p1, student_id_p2, cinsiyet_p1, cinsiyet_p2, grade_name_p1, grade_name_p2, gametype, school_name_p1, school_name_p2, GAME_NAME)
-                        stage2_data = get_stage_data(STAGE2.player2levels, gametype, 2)
-                        rows = get_rows(stage2_data, 1, NUM_LEVELS, competitive=True)
+                        stage2_data = get_stage_data(STAGE2.player1levels, gametype, 2)
+                        rows = get_rows(stage2_data, 2, NUM_LEVELS)
                         for exe_no, lst in rows.items():
-                            STAGE2DATA.append(student_data2 + lst)
-                    else:
-                        pass
-                        for exe_no, lst in rows.items():
-                            STAGE2DATA.pop()
+                            STAGE2DATA.append(student_data2 + lst)     
+                    elif gametype == 'competitive':
+                        if not all(i is None for i in STAGE2.player2levels) and not all(i is None for i in STAGE3.player2levels):
+                            student_data2 = get_init_row(game_ID, student_id_p1, student_id_p2, cinsiyet_p1, cinsiyet_p2, grade_name_p1, grade_name_p2, gametype, school_name_p1, school_name_p2, GAME_NAME)
+                            stage2_data = get_stage_data(STAGE2.player2levels, gametype, 2)
+                            rows = get_rows(stage2_data, 1, NUM_LEVELS, competitive=True)
+                            for exe_no, lst in rows.items():
+                                STAGE2DATA.append(student_data2 + lst)
+                        else:
+                            pass
+                            for exe_no, lst in rows.items():
+                                STAGE2DATA.pop()
 
-                student_data3 = get_init_row(game_ID, student_id_p1, student_id_p2, cinsiyet_p1, cinsiyet_p2, grade_name_p1, grade_name_p2, gametype, school_name_p1, school_name_p2, GAME_NAME)
-                stage3_data = get_stage_data(STAGE3.player1levels, gametype, 3)
-                rows = get_rows(stage3_data, 1, NUM_LEVELS)
-                for exe_no, lst in rows.items():
-                    STAGE3DATA.append(student_data3 + lst)     
-                if gametype == 'cooperative':
                     student_data3 = get_init_row(game_ID, student_id_p1, student_id_p2, cinsiyet_p1, cinsiyet_p2, grade_name_p1, grade_name_p2, gametype, school_name_p1, school_name_p2, GAME_NAME)
                     stage3_data = get_stage_data(STAGE3.player1levels, gametype, 3)
-                    rows = get_rows(stage3_data, 2, NUM_LEVELS)
+                    rows = get_rows(stage3_data, 1, NUM_LEVELS)
                     for exe_no, lst in rows.items():
                         STAGE3DATA.append(student_data3 + lst)     
-                elif gametype == 'competitive':
-                    if not all(i is None for i in STAGE2.player2levels) and not all(i is None for i in STAGE3.player2levels):
+                    if gametype == 'cooperative':
                         student_data3 = get_init_row(game_ID, student_id_p1, student_id_p2, cinsiyet_p1, cinsiyet_p2, grade_name_p1, grade_name_p2, gametype, school_name_p1, school_name_p2, GAME_NAME)
-                        stage3_data = get_stage_data(STAGE3.player2levels, gametype, 3)
-                        rows = get_rows(stage3_data, 1, NUM_LEVELS, competitive=True)
+                        stage3_data = get_stage_data(STAGE3.player1levels, gametype, 3)
+                        rows = get_rows(stage3_data, 2, NUM_LEVELS)
                         for exe_no, lst in rows.items():
-                            STAGE3DATA.append(student_data3 + lst)
-                    else:
-                        pass
-                        for exe_no, lst in rows.items():
-                            STAGE3DATA.pop()                  
-            
-            num_columns2 = len(student_data2) + len(lst)
-            if len(STAGE2DATA) and not all(i is None for i in STAGE2DATA[-1]):
-                STAGE2DATA.append([None] * num_columns2)
+                            STAGE3DATA.append(student_data3 + lst)     
+                    elif gametype == 'competitive':
+                        if not all(i is None for i in STAGE2.player2levels) and not all(i is None for i in STAGE3.player2levels):
+                            student_data3 = get_init_row(game_ID, student_id_p1, student_id_p2, cinsiyet_p1, cinsiyet_p2, grade_name_p1, grade_name_p2, gametype, school_name_p1, school_name_p2, GAME_NAME)
+                            stage3_data = get_stage_data(STAGE3.player2levels, gametype, 3)
+                            rows = get_rows(stage3_data, 1, NUM_LEVELS, competitive=True)
+                            for exe_no, lst in rows.items():
+                                STAGE3DATA.append(student_data3 + lst)
+                        else:
+                            pass
+                            for exe_no, lst in rows.items():
+                                STAGE3DATA.pop()                  
+                
+                num_columns2 = len(student_data2) + len(lst)
+                if len(STAGE2DATA) and not all(i is None for i in STAGE2DATA[-1]):
+                    STAGE2DATA.append([None] * num_columns2)
 
-            num_columns3 = len(student_data3) + len(lst)
-            if len(STAGE3DATA) and not all(i is None for i in STAGE3DATA[-1]):
-                STAGE3DATA.append([None] * num_columns3)
+                num_columns3 = len(student_data3) + len(lst)
+                if len(STAGE3DATA) and not all(i is None for i in STAGE3DATA[-1]):
+                    STAGE3DATA.append([None] * num_columns3)
+        except:
+            print(f'Error: {json_file}')
 
     
     df2 = export_stage_data(STAGE2DATA, stage2_data, "elma_s2.xlsx", student_data2, NUM_LEVELS, 2)
